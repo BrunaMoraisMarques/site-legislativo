55c02fbd47d6582e43687030c0037d434463cb7e
Bruna Morais Marques <seuemail@dominio.com>
2025-10-01 14:10:58 -0400

    Atualizar site para foco em leis e atos vinculados, incluindo detalhes e dados JS

---
+++ js/jsdados.js
@@ -0,0 +1,20 @@
+const leis = [
+  {
+    id: 1,
+    titulo: "Lei nº 001/2025 - Criação do Conselho Municipal",
+    descricao: "Descrição detalhada da lei 001/2025",
+    vinculados: [
+      "Decreto nº 010/2025",
+      "Portaria nº 005/2025",
+      "Lei nº 003/2025 (alteração)"
+    ]
+  },
+  {
+    id: 2,
+    titulo: "Lei nº 002/2025 - Regulação de Atividades Comerciais",
+    descricao: "Descrição detalhada da lei 002/2025",
+    vinculados: [
+      "Lei nº 004/2025 (revogação)"
+    ]
+  }
+];
